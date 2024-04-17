89cfe9f45015c90b3e9e38301a29921b70521f93
Takashi Ichihashi <ta.ichihashi@gmail.com>
2024-04-17 18:01:22 +0900

    initial commit

---
+++ VB_NET/VBTwinCATSample/Display.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Display
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        indicator_in_0 = New Label()
+        indicator_in_1 = New Label()
+        Label0 = New Label()
+        Label1 = New Label()
+        Timer1 = New Timer(components)
+        Label4 = New Label()
+        indicator_in_n1 = New Label()
+        Label5 = New Label()
+        indicator_in_string = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' indicator_in_0
+        ' 
+        indicator_in_0.AutoSize = True
+        indicator_in_0.Location = New Point(140, 33)
+        indicator_in_0.Name = "indicator_in_0"
+        indicator_in_0.Size = New Size(28, 15)
+        indicator_in_0.TabIndex = 0
+        indicator_in_0.Text = "OFF"
+        ' 
+        ' indicator_in_1
+        ' 
+        indicator_in_1.AutoSize = True
+        indicator_in_1.Location = New Point(140, 66)
+        indicator_in_1.Name = "indicator_in_1"
+        indicator_in_1.Size = New Size(28, 15)
+        indicator_in_1.TabIndex = 2
+        indicator_in_1.Text = "OFF"
+        ' 
+        ' Label0
+        ' 
+        Label0.AutoSize = True
+        Label0.Location = New Point(55, 32)
+        Label0.Name = "Label0"
+        Label0.Size = New Size(44, 15)
+        Label0.TabIndex = 1
+        Label0.Text = "Input 0"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(55, 65)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(44, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Input 1"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        Timer1.Interval = 1000
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(55, 113)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(50, 15)
+        Label4.TabIndex = 9
+        Label4.Text = "Number"
+        ' 
+        ' indicator_in_n1
+        ' 
+        indicator_in_n1.AutoSize = True
+        indicator_in_n1.Location = New Point(155, 113)
+        indicator_in_n1.Name = "indicator_in_n1"
+        indicator_in_n1.Size = New Size(13, 15)
+        indicator_in_n1.TabIndex = 8
+        indicator_in_n1.Text = "0"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(23, 185)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(53, 15)
+        Label5.TabIndex = 10
+        Label5.Text = "Message"
+        ' 
+        ' indicator_in_string
+        ' 
+        indicator_in_string.AutoSize = True
+        indicator_in_string.Location = New Point(23, 210)
+        indicator_in_string.Name = "indicator_in_string"
+        indicator_in_string.Size = New Size(0, 15)
+        indicator_in_string.TabIndex = 11
+        indicator_in_string.TextAlign = ContentAlignment.TopRight
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(23, 150)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(343, 23)
+        TextBox1.TabIndex = 12
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(244, 29)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 13
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(244, 61)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 14
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Display
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(378, 244)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(indicator_in_string)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(indicator_in_n1)
+        Controls.Add(Label1)
+        Controls.Add(indicator_in_1)
+        Controls.Add(Label0)
+        Controls.Add(indicator_in_0)
+        Name = "Display"
+        Text = "TwinCAT Input Display"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents indicator_in_0 As Label
+    Friend WithEvents Label0 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents indicator_in_1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label4 As Label
+    Friend WithEvents indicator_in_n1 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents indicator_in_string As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ VB_NET/VBTwinCATSample/Display.vb
@@ -0,0 +1,244 @@
+﻿Imports System.Buffers.Binary
+Imports TwinCAT.Ads
+Imports TwinCAT.TypeSystem
+
+
+Public Class Display
+    Private client As TwinCAT.Ads.AdsClient
+    Private num_result As UInteger
+    Private in1_result As UInteger
+    Private in2_result As UInteger
+    Private in3_result As UInteger
+    Private in4_result As UInteger
+    Private str1_handle As UInteger
+    Private str_result As UInteger
+    Private button2_status As Boolean
+    Private binReader As System.IO.BinaryReader
+    Private converter As PrimitiveTypeMarshaler ' this type is defined by TwincAT.TypeSystem
+
+
+    ''' <summary>
+    ''' Form起動時のイベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Diplay_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        ' ADS通信クライアントオブジェクト作成
+        client = New AdsClient()
+
+        ' Notificationイベントハンドラの追加
+        AddHandler client.AdsNotification, AddressOf Client_AdsNotification2
+
+        ' 文字型変数のバイト列を.NET環境のString型に変換するオブジェクト生成
+        converter = New PrimitiveTypeMarshaler(StringMarshaler.DefaultEncoding)
+
+        ' ターゲットPLCへの接続
+        client.Connect("127.0.0.1.1.1", 851)
+
+        ' Notification用設定オブジェクトの作成
+        ' Check for change every 100 ms
+        Dim notification_setting = New NotificationSettings(AdsTransMode.OnChange, 100, 0)
+
+        Try
+            ''' 変数の値変更イベント登録
+            ''' see https://infosys.beckhoff.com/content/1033/tc3_ads.net/9408448267.html?id=6017818919540761304
+            in1_result = client.AddDeviceNotification("MAIN.test_input[0]", 2, notification_setting, Nothing)
+            in2_result = client.AddDeviceNotification("MAIN.test_input[1]", 2, notification_setting, Nothing)
+            num_result = client.AddDeviceNotification("MAIN.test_value", 4, notification_setting, Nothing)
+
+            ''' 文字列処理に関するインスタンス設定
+            ''' 入力BOXとテキスト表示を連動させるため、次の3点の処理が必要。
+            ''' 1. 入力BOXの初期値を設定するためADSのReadValueによりテキストを読み出してString型へ変換
+            ''' 2. 入力BOXから入力された文字列をADSのWriteValueにより書込み
+            ''' 3. Notificationにより変数の文字列が変化したらテキスト表示部にその文字を反映
+            ''' 以上を使うことにより、Formからの文字入力、PLC側の制御で得た文字情報のどちらからもフォームへ反映させることができる。
+            ''' see https://infosys.beckhoff.com/content/1033/tc3_ads.net/9407532811.html?id=4153935062066595398
+            ' 変数の値変更イベント登録
+            str_result = client.AddDeviceNotification("MAIN.test_string", 255, notification_setting, Nothing)
+            ' 変数へ値書込み用のハンドル定義
+            str1_handle = client.CreateVariableHandle("MAIN.test_string")
+            ' PLC文字型変数の現在値をForm上の入力BOX初期値として設定
+            TextBox1.Text = GetCurrentString(str1_handle, 64)
+
+
+        Catch ex As TwinCAT.Ads.AdsErrorException
+            MessageBox.Show(ex.Message & Environment.NewLine & ex.HelpLink)
+            Application.Exit()
+        End Try
+
+
+    End Sub
+
+    ' ADS Notificationによるイベント処理スレッドからFormコントロールスレッドへ代理実行してもらうためのデリゲートを定義
+    Delegate Sub UpdateDisplayDelegate(e As AdsNotificationEventArgs)
+
+    ''' <summary>
+    ''' ADS Notificationによる イベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e">イベントハンドラ引数</param>
+    Private Sub Client_AdsNotification2(sender As Object, e As AdsNotificationEventArgs)
+        Try
+            Select Case e.Handle
+                ' 登録したイベント種別ごとに定義した処理をFormスレッドに実行を依頼する
+                Case num_result
+                    Invoke(New UpdateDisplayDelegate(AddressOf SetCounter), e)
+                Case in1_result
+                    Invoke(New UpdateDisplayDelegate(AddressOf SetIn1), e)
+                Case in2_result
+                    Invoke(New UpdateDisplayDelegate(AddressOf SetIn2), e)
+                Case str_result
+                    Invoke(New UpdateDisplayDelegate(AddressOf SetString), e)
+            End Select
+        Catch ex As System.ObjectDisposedException
+            ' フォームを閉じた時、Delegate objectがDisposeされたためinvokeに失敗する例外を握り潰す
+        End Try
+
+    End Sub
+
+    ''' <summary>
+    ''' "MAIN.test_value" : DINT の値変更イベントハンドラ
+    ''' </summary>
+    ''' <param name="e">イベントハンドラ引数</param>
+    Sub SetCounter(e As AdsNotificationEventArgs)
+        ' ネイティブバイトオーダへ変換
+        Dim nCounter As UInteger = BinaryPrimitives.ReadUInt32LittleEndian(e.Data.Span)
+        indicator_in_n1.Text = nCounter.ToString()
+    End Sub
+
+    ''' <summary>
+    ''' "MAIN.test_input[0]" : BOOL の値変更イベントハンドラ
+    ''' Formのindicator_in_0のテキストに表示反映する
+    ''' </summary>
+    ''' <param name="e">イベントハンドラ引数</param>
+    Sub SetIn1(e As AdsNotificationEventArgs)
+        ' ネイティブバイトオーダへ変換
+        Dim input As UShort = BinaryPrimitives.ReadUInt16LittleEndian(e.Data.Span)
+        ' Byteサイズに切り詰める
+        input = input And &HF
+        If input > 0 Then
+            indicator_in_0.Text = "ON"
+            indicator_in_0.BackColor = Color.Yellow
+        Else
+            indicator_in_0.Text = "OFF"
+            indicator_in_0.BackColor = Nothing
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' "MAIN.test_input[1]" : BOOL の値変更イベントハンドラ
+    ''' Formのindicator_in_1のテキストに表示反映する
+    ''' </summary>
+    ''' <param name="e">イベントハンドラ引数</param>
+    Sub SetIn2(e As AdsNotificationEventArgs)
+        ' ネイティブバイトオーダへ変換
+        Dim input As UShort = BinaryPrimitives.ReadUInt16LittleEndian(e.Data.Span)
+        ' Byteサイズに切り詰める
+        input = input And &HF
+        If input > 0 Then
+            indicator_in_1.Text = "ON"
+            indicator_in_1.BackColor = Color.Yellow
+        Else
+            indicator_in_1.Text = "OFF"
+            indicator_in_1.BackColor = Nothing
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' MAIN.test_string" : STRING(64) の値変更イベントハンドラ
+    ''' Formのindicator_in_stringへテキストを反映する
+    ''' </summary>
+    ''' <param name="e">イベントハンドラ引数</param>
+    Sub SetString(e As AdsNotificationEventArgs)
+        Dim read_text As String
+        'バイト配列を文字型へ変換して
+        converter.Unmarshal(e.Data.Span, read_text)
+        indicator_in_string.Text = read_text
+        TextBox1.Text = read_text
+    End Sub
+
+    ''' <summary>
+    ''' PLCの文字列型変数のテキストを読み取る
+    ''' </summary>
+    ''' <param name="TcHandle">文字列変数のハンドラ</param>
+    ''' <param name="TextSize">TwinCATの文字列変数のサイズn STRING(n) </param>
+    ''' <returns>読みだしたテキスト</returns>
+    Private Function GetCurrentString(TcHandle As UInteger, TextSize As UInteger) As String
+        Dim Buffer As Byte() = New Byte(TextSize) {}
+        Dim readBytes As Integer = client.Read(TcHandle, Buffer.AsMemory())
+        If readBytes > 0 Then
+            converter.Unmarshal(Buffer.AsSpan(), GetCurrentString)
+        End If
+    End Function
+
+    ''' <summary>
+    ''' テキスト入力BOXのテキストが変更されたイベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim TextSize As UInteger = 64
+        If Len(sender.Text) <= TextSize Then
+            Dim writeBuffer As Byte() = New Byte(TextSize) {}
+            converter.Marshal(sender.Text, writeBuffer.AsSpan())
+            client.Write(str1_handle, writeBuffer.AsMemory())
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' フォームを閉じる際のイベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Display_Closing(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Closing
+        client.DeleteDeviceNotification(num_result)
+        client.DeleteDeviceNotification(in1_result)
+        client.DeleteDeviceNotification(in2_result)
+
+        If client IsNot Nothing Then
+            RemoveHandler client.AdsNotification, AddressOf Client_AdsNotification2
+        End If
+    End Sub
+
+    ' Outputs
+
+    ''' <summary>
+    ''' モメンタリボタンの押した時のイベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_MouseDown(sender As Object, e As EventArgs) Handles Button1.MouseDown
+        client.WriteValue("MAIN.test_output[0]", True)
+        Button1.BackColor = Color.Yellow
+    End Sub
+
+    ''' <summary>
+    ''' モメンタリボタンの離した時のイベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_MouseUp(sender As Object, e As EventArgs) Handles Button1.MouseUp
+        client.WriteValue("MAIN.test_output[0]", False)
+        Button1.BackColor = Nothing
+    End Sub
+
+    ''' <summary>
+    ''' オルタネートボタンのクリック時のイベントハンドラ
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If button2_status Then
+            button2_status = False
+            Button2.BackColor = Nothing
+        Else
+            button2_status = True
+            Button2.BackColor = Color.Yellow
+        End If
+        client.WriteValue("MAIN.test_output[1]", button2_status)
+    End Sub
+
+End Class
